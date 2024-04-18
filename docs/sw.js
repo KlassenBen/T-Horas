85202272e3f6a39558eef2264440fae903f80708
Ben <benklassen19@icloud.com>
2024-04-17 20:14:27 -0600

    new

---
+++ docs/sw.js
@@ -0,0 +1,49 @@
+const cacheName = "THoras";
+const staticAssets = [
+  " ./index.html",
+  "./index.css",
+  "./index.js",
+  "./manifest.webmanifest",
+  "./images",
+];
+self.addEventListener("install", async (e) => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", async (e) => {
+  const req = e.request;
+  if (req.url.includes("google")) {
+  } else {
+    const url = new URL(req.url);
+    if (url.origin === location.origin) {
+      // e.respondWith(cacheFirst(req));
+      e.respondWith(networkAndCache(req));
+    } else {
+      e.respondWith(networkAndCache(req));
+    }
+  }
+});
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cached = await cache.match(req);
+  return cached || fetch(req);
+}
+
+async function networkAndCache(req) {
+  const cache = await caches.open(cacheName);
+  try {
+    const fresh = await fetch(req);
+    await cache.put(req, fresh.clone());
+    return fresh;
+  } catch (e) {
+    const cached = await cache.match(req);
+    return cached;
+  }
+}
